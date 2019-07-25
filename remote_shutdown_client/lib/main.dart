cf11a323929970951731255d2a02274e7c89c8fb
CookieSilent <benf3632@gmail.com>
2019-07-25 18:06:58 +0300

    Change App name and added a link to desktop app

--- remote_shutdown_client/lib/main.dart
+++ remote_shutdown_client/lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:ui' as prefix0;
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'socket.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 Duration _time = new Duration(seconds: 0);
@@ -219,12 +223,33 @@ class Help extends StatelessWidget {
         title: new Text("Help"),
         centerTitle: true,
       ),
-      body: new Row(
+      body: new Column(
         children: <Widget>[
-          new SizedBox(width: 20.0),
-          new Text("To start the shutdown timer:\n1.pick a time with the button 'Pick Time'\n2.pick a mode (shutdown, restart, hibernate)\n3.Enter the IP of the computer to shutdown\n (can be seen in the option on the computer 'Show IP'\n4.Click Start\n\nNote: Need to be connected to the same network"),
-        ], 
-      ),
+          new Row(
+            children: <Widget>[
+              new SizedBox(width: 20.0),
+              new Text("To start the shutdown timer:\n1.pick a time with the button 'Pick Time'\n2.pick a mode (shutdown, restart, hibernate)\n3.Enter the IP of the computer to shutdown\n (can be seen in the option on the computer 'Show IP'\n4.Click Start\n\nNote: Need to be connected to the same network"),
+            ], 
+          ),
+          new SizedBox(height: 16.0),
+          new RichText(
+            text: new TextSpan(
+              children: [
+                new TextSpan(
+                  text: 'The Dekstop Application can be found at:\n',
+                  style: new TextStyle(color: Colors.black)
+                ),
+                new TextSpan(
+                  text: '     Github: RemoteShutdown-Releases',
+                  style: new TextStyle(color: Colors.blue),
+                  recognizer: new TapGestureRecognizer()
+                    ..onTap = () { launch('https://github.com/benf3632/RemoteShutdown-Releases/releases');}
+                )
+              ]
+            ),
+          )
+        ],
+      )
     );
   }
 }
